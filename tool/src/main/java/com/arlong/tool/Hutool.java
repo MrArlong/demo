9fa0af4479d50d7cb603fd42bc5a59ed0c683f72
张占龙 <16075085@qq.com>
2023-07-27 18:00:33 +0800

    增加http请求

--- tool/src/main/java/com/arlong/tool/Hutool.java
+++ tool/src/main/java/com/arlong/tool/Hutool.java
@@ -3,11 +3,11 @@ package com.arlong.tool;
 import cn.hutool.core.util.NumberUtil;
 
 /**
- * @description:
+ * @description: hutool常用方法
  * @author: zzl
  * @date: 2023/7/26 14:04
  **/
-public class hutool {
+public class Hutool {
     public static void main(String[] args) {
         // 计算百分数
         String percentage = NumberUtil.decimalFormat("#.##%", NumberUtil.div(100, 1000));
